c7a2bd065ac780ad5106f7a41d9c23f43ba88719
Matti Astrand <mattiastr@gmail.com>
2018-01-14 11:24:42 +0000

    xmobar, Xres

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -3,11 +3,20 @@ if [[ ! -e $HOME/.config/nvim ]]; then
     ln -s $CURRENTDIR/nvim $HOME/.config/nvim
 fi
 
-
+# git
 if [[ ! -e $HOME/.gitconfig ]]; then
     ln -s $CURRENTDIR/git/gitconfig $HOME/.gitconfig
 fi
-
 if [[ ! -e $HOME/.gitignore_global ]]; then
     ln -s $CURRENTDIR/git/gitignore_global $HOME/.gitignore_global
+fi
+
+# core
+if [[ ! -e $HOME/.Xresources ]]; then
+    ln -s $CURRENTDIR/core/Xresources $HOME/.Xresources
+fi
+
+# xmonad
+if [[ ! -e $HOME/.xmobarrc ]]; then
+    ln -s $CURRENTDIR/xmonad/xmobarrc $HOME/.xmobarrc
 fi
