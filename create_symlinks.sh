2feb85a2e5ef9b92f2a6302619e7dbbf83faacbf
Matti Astrand <mattiastr@gmail.com>
2018-01-14 10:26:01 +0000

    git configs

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,2 +1,13 @@
 CURRENTDIR=$(pwd -P)
-ln -s $CURRENTDIR/nvim $HOME/.config/nvim
+if [[ ! -e $HOME/.config/nvim ]]; then
+    ln -s $CURRENTDIR/nvim $HOME/.config/nvim
+fi
+
+
+if [[ ! -e $HOME/.gitconfig ]]; then
+    ln -s $CURRENTDIR/gitconfig $HOME/.gitconfig
+fi
+
+if [[ ! -e $HOME/.gitignore_global ]]; then
+    ln -s $CURRENTDIR/gitignore_global $HOME/.gitignore_global
+fi
